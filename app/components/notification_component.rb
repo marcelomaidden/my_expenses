46be49b8e5eb63cd84ce9f52de354e4cc798b1e7
Marcelo Fernandes de Araújo <marcelomaidden@gmail.com>
2022-04-16 18:17:55 -0300

    Adds view_component and notification component

---
+++ app/components/notification_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class NotificationComponent < ViewComponent::Base
+  attr_reader :message, :type, :title
+
+  def initialize(message:, type:, title:)
+    @message = message
+    @type = type
+    @title = title
+  end
+
+  def notification_background
+    case type
+    when "notice"
+      "alert alert-warning"
+    when "danger"
+      "alert alert-danger"
+    when "alert"
+      "alert alert-danger"
+    else
+      "alert alert-dark"
+    end
+  end
+end
