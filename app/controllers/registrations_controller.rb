9a5f9f39d9d1795d1a9aeaa362f6281478d9c38c
Marcelo Fernandes de Araújo <marcelomaidden@gmail.com>
2022-04-17 11:18:36 -0300

    Devise Registration and Session
    - Changes registration and session routes
    - Adds SessionsController and RegistrationsController
    - Changes Registration form layout

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,9 @@
+class RegistrationsController < Devise::RegistrationsController
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
+end
